53a0e86d85d08e6bda913ba315828f7f5b7cabc4
Kyle Walters <kyle.p.walters@gmail.com>
2020-06-16 06:20:45 +0000

    Automatically backed up by Learn

--- lib/03_third_challenge.rb
+++ lib/03_third_challenge.rb
@@ -2,7 +2,7 @@ def third_challenge
   epic_tragedy = {
    :montague => {
       :patriarch => {
-        :name => "Lord Montague"
+        :name => "Lord Montague",
         :age => "53"
         },
       :matriarch => {
@@ -14,8 +14,7 @@ def third_challenge
         :age => "15",
         :status => "alive"
         },
-      :hero_friends => []
-   }, 
+      :hero_friends => [],
    :capulet => {
       :patriarch => {
         :name => "Lord Capulet",
