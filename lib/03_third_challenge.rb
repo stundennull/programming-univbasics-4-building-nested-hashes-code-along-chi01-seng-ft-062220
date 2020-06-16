9046e3cb2e4670355e2ae61b1ea8e19944054990
Kyle Walters <kyle.p.walters@gmail.com>
2020-06-16 06:19:05 +0000

    Automatically backed up by Learn

--- lib/03_third_challenge.rb
+++ lib/03_third_challenge.rb
@@ -23,7 +23,7 @@ def third_challenge
         },
       :matriarch => {
         :name => "Lady Capulet",
-        :age "51"
+        :age => "51"
         },
       :heroine => {
         :name => "Juliet",
